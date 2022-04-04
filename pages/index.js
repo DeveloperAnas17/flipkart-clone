b9bcdd7a653e9ba6a235e23d11dcc09f7875af8f
Anas <amanabbasi3660@gmail.com>
2022-04-04 15:48:17 +0530

    ...

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,4 @@
 import Head from "next/head";
-import Banner from "../components/Banner";
-import Books from "../components/Books";
-import Catgories from "../components/Catgories";
-import Electronics from "../components/Electronics";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import Laptops from "../components/Laptops";
-import MenFashion from "../components/MenFashion";
-import Mobiles from "../components/Mobiles";
-import Trending from "../components/Trending";
 
 export default function Home() {
   return (
@@ -17,18 +7,6 @@ export default function Home() {
         <title>Flipkart Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
-      <Catgories />
-      <div className="max-w-[1600px] mx-auto space-y-2">
-        <Banner />
-        <Trending />
-        <Electronics />
-        <Books />
-        <Laptops />
-        <Mobiles />
-        <MenFashion />
-      </div>
-      <Footer />
     </div>
   );
 }
